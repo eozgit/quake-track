1e82f93553404e500af7a88d3031e1f9dc5dad9c
Enis Ozgit <eozgit@gmail.com>
2020-05-18 11:50:49 +0100

    Configure JSON serialization
    Add Newtonsoft JSON package
    Set serializer reference loop handling to ignore

--- Startup.cs
+++ Startup.cs
@@ -56,6 +56,11 @@ namespace QuakeTrack
             {
                 configuration.RootPath = "ClientApp/dist";
             });
+
+            services.AddControllersWithViews()
+                .AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+            );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
